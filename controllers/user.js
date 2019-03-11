8b55868161ec5bdd5ceafbe2ea3c90b27bc72935
tanmayiitk <tanmayy.iitk@gmail.com>
2019-03-11 18:31:33 +0530

    Email api activated

--- controllers/user.js
+++ controllers/user.js
@@ -315,8 +315,8 @@ exports.postReset = (req, res, next) => {
     });
     const mailOptions = {
       to: user.email,
-      from: 'hackathon@starter.com',
-      subject: 'Your Hackathon Starter password has been changed',
+      from: 'THE_IKIGAI_LABs',
+      subject: 'Your THE_IKIGAI_LABs password has been changed',
       text: `Hello,\n\nThis is a confirmation that the password for your account ${user.email} has just been changed.\n`
     };
     return transporter.sendMail(mailOptions)
@@ -411,7 +411,7 @@ exports.postForgot = (req, res, next) => {
     const mailOptions = {
       to: user.email,
       from: 'THE_IKIGAI_LAB@ikigai.com',
-      subject: 'Reset your password on Hackathon Starter',
+      subject: 'Reset your password for THE_IKIGAI_LAB',
       text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
         http://${req.headers.host}/reset/${token}\n\n
