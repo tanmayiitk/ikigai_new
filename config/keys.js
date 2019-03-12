5473dd13c55954e7d8c6e6995a4404b94d6c9f9c
tanmayiitk <tanmayy.iitk@gmail.com>
2019-03-12 20:26:47 +0530

    #

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-module.exports={
-		SENDGRID_USER: process.env.SENDGRID_USER,
-		SENDGRID_PASSWORD:process.env.SENDGRID_PASSWORD
-};
+// module.exports={
+// 		SENDGRID_USER: process.env.SENDGRID_USER,
+// 		SENDGRID_PASSWORD:process.env.SENDGRID_PASSWORD
+// };
